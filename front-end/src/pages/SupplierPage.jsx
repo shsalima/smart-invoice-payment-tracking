ac1d68edb75d542eecb58975082baabda31c9830
salima <sahisalima77@gmail.com>
2026-05-13 17:58:47 +0200

    travil sur page de afficher détail d-un fournisseur

--- front-end/src/pages/SupplierPage.jsx
+++ front-end/src/pages/SupplierPage.jsx
@@ -2,6 +2,7 @@ import AjouterSupplier from "../components/AjouterSupplier";
 import SupplierCart from "../components/suppliers/SupplierCart";
 import SupplierHero from "../components/suppliers/SupplierHero";
 import { useEffect, useState } from "react";
+import SupplierDetailPage from "./SupplierDetailPage";
 
 
 export default function SupplierPage(){
@@ -78,6 +79,7 @@ export default function SupplierPage(){
         <div className="min-h-screen bg-[#F9F9F8] p-8">
             <SupplierHero loading={loading}/>
             
+            {/* Hna fin drna l-grid bach les cartes i-jiw m-stfin fhal image_2c9b34.png */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
                 {suppliers.map((supplier)=>(
                     <SupplierCart key={supplier._id} supplier={supplier}/>
@@ -85,6 +87,7 @@ export default function SupplierPage(){
             </div>
 
             {/* <AjouterSupplier ... /> */}
+            <SupplierDetailPage/>
         </div>
     )
 }
