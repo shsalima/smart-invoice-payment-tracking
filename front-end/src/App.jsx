b9b236c0d002c63e0d54e719971001d43aceb7db
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-05-13 15:10:42 +0100

    Finished up the app layout: side bare + header

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,22 @@
+import { Routes, Route } from "react-router";
+import SideBar from "./components/layout/SideBar";
+import AppHeader from "./components/layout/AppHeader";
+import HomePage from "./pages/HomePage";
+
 function App() {
     return (
-        <>
-            <h1>Hello World!</h1>
-        </>
+        <div className="flex">
+            <SideBar />
+
+            <div className="flex-1">
+                <AppHeader />
+                <div className="container py-6">
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                    </Routes>
+                </div>
+            </div>
+        </div>
     );
 }
 
---
+++ front-end/src/components/layout/AppHeader.jsx
@@ -0,0 +1,7 @@
+export default function AppHeader() {
+    return (
+        <header className="container bg-white w-full py-4 border-b border-[#E2DDD8]">
+            <h2>Dashboard</h2>
+        </header>
+    );
+}
