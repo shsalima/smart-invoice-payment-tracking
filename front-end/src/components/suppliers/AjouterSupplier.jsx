217e5c2ca3d1070b33708962d60e31cd88cb9242
salima <sahisalima77@gmail.com>
2026-05-15 23:33:17 +0200

    travail sur la concemeantion des api dans frant-end et afficher toute les fournisseur

--- front-end/src/components/suppliers/AjouterSupplier.jsx
+++ front-end/src/components/suppliers/AjouterSupplier.jsx
@@ -1,6 +1,43 @@
+import  axios  from 'axios';
+import { useContext ,useState} from 'react';
+import { UserContext } from '../../contexts/UserContext';
 
 
-export default function AjouterSupplier({isOpen,onClose}){
+export default function AjouterSupplier({isOpen,onClose,onSupplierAdded}){
+    const {VITE_API_URL}= import.meta.env
+    const [formData,setFormData]=useState({name:"" })
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(null);
+    const {accessToken}=useContext(UserContext)
+
+    if(!isOpen) return null;
+    function handleChange(e){
+        setFormData({name:e.target.value})
+
+    }
+    async function handleSubmit(e){
+        e.preventDefault();
+        setLoading(true);
+        setErrorMessage(null);
+        try{
+            
+            const response=await axios.post(`${VITE_API_URL}/suppliers`,formData,{
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+            if(onSupplierAdded){
+                onSupplierAdded(response.data);
+            }
+            onClose();
+        } catch (error) {
+            console.error("Error adding supplier:", error);
+            setErrorMessage(error.response?.data?.message || "Erreur lors de l'ajout.");
+
+        }finally{
+            setLoading(false);
+        }
+    }
     return(
         <div className="fixed inset-0 bg-[#00000078] bg-opacity-50 flex items-center justify-center p-4">
             <div className="bg-white rounded-3xl w-full max-w-2xl p-8 shadow-2-xl relative">
@@ -8,18 +45,23 @@ export default function AjouterSupplier({isOpen,onClose}){
                   <span className="text-xl px-1">×</span>
                 </button>
                 <h2 className="text-2xl font-bold text-gray-900 mb-8">New Supplier</h2>
-                <form className="space-y-6">
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                        <div>
-                            <label className="block text-sm font-semibold text-grap-700 mb-2">Company Name</label>
+                {errorMessage && (
+                    <p className="text-red-500 mb-4 text-sm">*{errorMessage}</p>
+                )}
+                <form onSubmit={handleSubmit} className="space-y-6">
+                    <div>
+                        <label className="block text-sm font-semibold text-gray-700 mb-2">Company Name</label>
                             <input 
-                              type="text"
-                              placeholder="ACME Corp"
-                              className="w-full px-4 py-3 rounded-xl border border-gray-200 focus:outline-none focus:ring-2 focus:ring-emerald-500 bg-gray-50"
-                              required
+                             type="text"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            placeholder="ACME Corp"
+                            className="w-full px-4 py-3 rounded-xl border border-gray-200 focus:outline-none focus:ring-2 focus:ring-emerald-500 bg-gray-50"
+                            required
                               />
                         </div>
-                        <div>
+                        {/* <div>
                             <label className="block text-sm font-semibold text-gray-700 mb-2">Category</label>
                             <input 
                                 type="text"
@@ -50,24 +92,25 @@ export default function AjouterSupplier({isOpen,onClose}){
                                 type="date"
                                 className="w-full px-4 py-3 rounded-xl border border-gray-200 focus:outline-none focus:ring-2 focus:ring-emerald-500 bg-gray-50"
                             />
-                            </div>
-                            <div className="flex gap-4 pt-4">
-                                <button
-                                type="submit"
-                                className="bg-[#2D6A4F] text-white px-8 py-3 rounded-xl font-bold hover:bg-[#1B4332] transition-colors"
-                                >
-                                Save
-                                </button>
-                                <button 
-                                type="button"
-                                onClick={onClose}
-                                className="border border-gray-200 text-gray-600 px-8 py-3 rounded-xl font-bold hover:bg-gray-50 transition-colors"
-                                >
-                                Cancel
-                                </button>
-                            </div>
-                        </div>
+                            </div> */}
+                            <div className="flex gap-4 pt-4 justify-end">
+                        <button 
+                            type="button"
+                            onClick={onClose}
+                            className="border border-gray-200 text-gray-600 px-6 py-2.5 rounded-xl font-bold hover:bg-gray-50 transition-colors"
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            type="submit"
+                            disabled={loading}
+                            className="bg-[#2D6A4F] text-white px-6 py-2.5 rounded-xl font-bold hover:bg-[#1B4332] transition-colors disabled:bg-gray-400"
+                        >
+                            {loading ? "Saving..." : "Save"}
+                        </button>
                     </div>
+                        {/* </div>
+                    </div> */}
 
                 </form>
 
--- front-end/src/pages/SupplierPage.jsx
+++ front-end/src/pages/SupplierPage.jsx
@@ -1,71 +1,67 @@
 import AjouterSupplier from "../components/suppliers/AjouterSupplier";
 import SupplierCart from "../components/suppliers/SupplierCart";
 import SupplierHero from "../components/suppliers/SupplierHero";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import SupplierDetailPage from "./SupplierDetailPage";
+import  axios  from 'axios';
+import { UserContext } from "../contexts/UserContext";
 
 export default function SupplierPage() {
-  const suppliersData = [
-    {
-      _id: "69d61ff53a6fce13b876006c",
-      name: "supplier 1",
-      clientId: "69d3cab25da90a00b6953397",
-      createdAt: "2026-04-08T09:29:25.537Z",
-      __v: 0,
-    },
-    {
-      _id: "69d9046ce7b87b8b0604c3e3",
-      name: "supplier 2",
-      clientId: "69d3cab25da90a00b6953397",
-      createdAt: "2026-04-10T14:08:44.080Z",
-      __v: 0,
-    },
-    {
-      _id: "69d90631e7b87b8b0604c3e4",
-      name: "supplier 4",
-      clientId: "69d3cab25da90a00b6953397",
-      createdAt: "2026-04-10T14:16:17.229Z",
-      __v: 0,
-    },
-    {
-      _id: "69d90636e7b87b8b0604c3e5",
-      name: "supplier 5",
-      clientId: "69d3cab25da90a00b6953397",
-      createdAt: "2026-04-10T14:16:22.501Z",
-      __v: 0,
-    },
-    {
-      _id: "69d90744af00fa5f477590b3",
-      name: "supplier 6",
-      clientId: "69d3cab25da90a00b6953397",
-      createdAt: "2026-04-10T14:20:52.883Z",
-      __v: 0,
-    },
-  ];
+  const {VITE_API_URL}= import.meta.env
 
   const [suppliers, setSuppliers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const[errorMessage,setErrorMessage]=useState(null)
+  const {accessToken}=useContext(UserContext)
+  // console.log(accessToken,"fff")
 
   useEffect(() => {
-    setSuppliers(suppliersData);
-    setLoading(false);
-  }, []);
+    async function fetchSuppliers() {
+      try{
+        
+        const response=await axios.get(`${VITE_API_URL}/suppliers`,{
+          headers:{
+            Authorization: `Bearer ${accessToken}`
+          }
+      })
+      setSuppliers(response.data.suppliers);
+      console.log(response.data)
+      }catch(error){
+        console.error("Error fetching suppliers:", error);
+        setErrorMessage('Impossibe de charger les fournisseurs.')
+    }finally{
+        setLoading(false);
+    }
+   
+  }
+  fetchSuppliers();
+},[]
+);
 
   return (
     <div className="min-h-screen bg-[#F9F9F8] p-8">
       <SupplierHero loading={loading} openModel={() => setIsModalOpen(true)} />
-
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
-        {suppliers.map((supplier) => (
-          <SupplierCart key={supplier._id} supplier={supplier} />
-        ))}
-      </div>
+      {errorMessage && (
+        <p className="text-red-500 mt-4 text-center">*{errorMessage}</p>
+      )}
+      {loading ? (
+        <p className="text-center mt-8 text-gray-500">Chargement en cours...</p>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
+          {suppliers.length? suppliers.map((supplier) => (
+            <SupplierCart key={supplier._id} supplier={supplier} />
+          )):
+          <p>rah makayn 7ta supplier</p>
+          }
+        </div>
+      )}
 
       {isModalOpen && (
         <AjouterSupplier
           onClose={() => setIsModalOpen(false)}
           isOpen={isModalOpen}
+          onSupplierAdded={(newSupplier) => setSuppliers((prev) => [newSupplier, ...prev])}
         />
       )}
       
