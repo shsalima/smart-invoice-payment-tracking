dff74889f22ff9aabd861c953f4be095a4a25a62
elhajjame <el-hajjame@outlook.com>
2026-05-15 18:00:47 +0100

    getting data from backend

--- front-end/src/pages/InvoicePage.jsx
+++ front-end/src/pages/InvoicePage.jsx
@@ -1,7 +1,13 @@
+import { useContext } from "react";
 import { InvoiceHeader } from "../components/InvoicesComponents/invoiceHeader";
 import InvoiceTable from "../components/InvoicesComponents/InvoicesTable";
+import { UserContext } from "../contexts/UserContext";
 
 export function InvoicePage() {
+  const { accessToken } = useContext(UserContext);
+
+  console.log(accessToken);
+
   return (
     <div>
       <InvoiceHeader />
